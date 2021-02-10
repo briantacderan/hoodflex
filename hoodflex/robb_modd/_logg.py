20e47530a2f66722a1c5034bbb3b2b6cf4820c50
Brian Tacderan <briantacderan@gmail.com>
2021-02-10 05:19:45 -0800

    Edit logg module

--- hoodflex/robb_modd/_logg.py
+++ hoodflex/robb_modd/_logg.py
@@ -15,7 +15,7 @@ class WidgetForecaster(GradientIterator):
     def __init__(self, ticker, start, date_points, **kwargs):
         super().__init__(ticker, start, date_points, **kwargs)
         self.plt_value_1 = 0.0
-        self.plt_value_2 = 37.0
+        self.plt_value_2 = 18.0
         self.plt_label_1 = '11/10/2020'
         self.plt_label_2 = '11/10/2022'
         
@@ -78,11 +78,11 @@ class WidgetForecaster(GradientIterator):
            
     def hoodflex_widget(self):
         dates_1yr = list([self.start + dt.timedelta(days=i) for i in range(366)])
-        tick_options = [(dates_1yr[i].strftime('%m/%d/%Y'), i/10) for i in range(len(dates_1yr)) if i % 5 == 0]
+        tick_options = [(dates_1yr[i].strftime('%m/%d/%Y'), i/20) for i in range(len(dates_1yr)) if i % 10 == 0]
         
         date_slider = SelectionRangeSlider(
             options=tick_options,
-            index=(0, 73),
+            index=(0, 36),
             description='Date:',
             disabled=False
         )
