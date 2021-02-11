38a91b24f1e74bfba54e380a104a27e005ac8ced
Brian Tacderan <briantacderan@gmail.com>
2021-02-10 21:12:40 -0800

    Edit logg module

--- hoodflex/robb_modd/_logg.py
+++ hoodflex/robb_modd/_logg.py
@@ -20,7 +20,7 @@ class WidgetForecaster(GradientIterator):
         
     def new_axis_values(self, b, m):
         range_X = 9.5
-        new_X = list(self.x)
+        new_X = list(self.x*2)
         new_X.append(range_X)
         new_Y = list(self.y)
         two_year_future = m*range_X + b
